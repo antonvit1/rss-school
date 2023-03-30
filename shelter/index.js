3f3304708d4b59270a5768e79e6cdc7fdc1ed273
Anton Viter <antonbntu@gmail.com>
2023-03-31 00:39:05 +0300

    feat: add burger menu

--- shelter/index.js
+++ shelter/index.js
@@ -8,3 +8,11 @@ console.log(`Страница Main (60)\n
 Вёрстка соответствует макету +15\n
 Требования к css +4
 Интерактивность элементов +14\n`);
+
+let iconBurgerMenu = document.querySelector('.icon-burger-menu');
+let burgerMenu = document.querySelector('.burger-menu');
+
+iconBurgerMenu.addEventListener('click',function(){
+    iconBurgerMenu.classList.toggle('active');
+    burgerMenu.classList.toggle('active');
+})

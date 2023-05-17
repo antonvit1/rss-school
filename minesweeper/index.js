4e0f0b5a25bfce25f29d7dd7fd9bbc94feae49e7
Anton Viter <antonbntu@gmail.com>
2023-05-17 23:54:40 +0300

    feat: add sound and add fild 15 cells and 25 cells

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -1,10 +1,12 @@
-let countBomb = 10;
+let countBomb = 5;
 let flag = 0;
+let checkSound = true;
 
-const width = 10;
+let width = 10;
 const arrayComplexity = ["Easy", "Medium", "Hard"];
 const body = document.querySelector("body");
 body.classList = "body";
+
 
 //create inscription about count flag
 let numberOfRemainBombs = countBomb;
@@ -26,19 +28,106 @@ let mesageNoFlag = document.createElement("span");
 mesageNoFlag.className = "message-no-flag";
 wrapperInscriptionCountFlag.appendChild(mesageNoFlag);
 
-let arrayOfCellAll = Array(width * width).fill(" ");
-let arrayOfCell = [];
-for (let i = 0; i < arrayOfCellAll.length / width; i++) {
-  arrayOfCell[i] = arrayOfCellAll.slice(i * width, i * width + width);
-}
-
 const wrapperOfWrapperComplexity = document.createElement("div");
 wrapperOfWrapperComplexity.className = "wrapper-of-wrapper";
 body.appendChild(wrapperOfWrapperComplexity);
 const divCell = document.createElement("div");
 divCell.className = "all-cells";
+divCell.style.gridTemplateColumns = `repeat(${width}, 25px)`;
+
+
+// checkbox
+const wrapperComplexity = document.createElement("div");
+wrapperComplexity.className = "complexity-wrapper";
+body.appendChild(wrapperComplexity);
+
+arrayComplexity.forEach((value) => {
+  const complexity = document.createElement("input");
+  const textComplexity = document.createElement("label");
+  complexity.type = "radio";
+  complexity.name = "complexity";
+  complexity.className = "buttonComplexity";
+  textComplexity.className = "buttonComplexityText";
+  textComplexity.innerHTML = value;
+  wrapperComplexity.appendChild(complexity);
+  wrapperComplexity.appendChild(textComplexity);
+});
+
+let arrOfDomComplexity = Array.from(document.querySelectorAll(".buttonComplexityText"));
+arrOfDomComplexity.forEach((choice) => {
+
+  choice.addEventListener("click", () => {
+  if (choice.innerHTML === "Easy") {
+
+    width = 10;
+    numberOfRemainBombs = countBomb;
+    countFlag.innerHTML = numberOfRemainBombs;
+
+    while(divCell.firstChild){
+      divCell.removeChild(divCell.firstChild)
+      }
+    writtenDom()
+    fieldFilling();
+    openCell();
+
+    divCell.style.gridTemplateColumns = `repeat(${width}, 25px)`;
+
+  } else if (choice.innerHTML === "Medium") {
+    width = 15;
+    numberOfRemainBombs = countBomb;
+    countFlag.innerHTML = numberOfRemainBombs;
+
+    while(divCell.firstChild){
+    divCell.removeChild(divCell.firstChild)
+    }
+    writtenDom()
+    fieldFilling();
+    openCell();
+    divCell.style.gridTemplateColumns = `repeat(${width}, 25px)`;
+
+  } else if (choice.innerHTML === "Hard") {
+    width = 25;
+    numberOfRemainBombs = countBomb;
+    countFlag.innerHTML = numberOfRemainBombs;
+
+    while(divCell.firstChild){
+      divCell.removeChild(divCell.firstChild)
+      }
+    writtenDom()
+    fieldFilling();
+    openCell();
+    divCell.style.gridTemplateColumns = `repeat(${width}, 25px)`;
+
+  }
+  });
+});
+
+
+
+
+
+const soundOnOff = document.createElement("img");
+soundOnOff.className = "sound-on-off";
+soundOnOff.src = `./assets/img/sound-on.svg`;
+wrapperInscriptionCountFlag.appendChild(soundOnOff);
+
+soundOnOff.addEventListener("click", () => {
+  if (checkSound) {
+    soundOnOff.src = `./assets/img/sound-off.svg`;
+    checkSound = false;
+  } else {
+    soundOnOff.src = `./assets/img/sound-on.svg`;
+    checkSound = true;
+  }
+});
 
 wrapperOfWrapperComplexity.appendChild(divCell);
+function writtenDom() {
+let arrayOfCellAll = Array(width * width).fill(" ");
+let arrayOfCell = [];
+for (let i = 0; i < arrayOfCellAll.length / width; i++) {
+  arrayOfCell[i] = arrayOfCellAll.slice(i * width, i * width + width);
+}
 arrayOfCell.forEach((element) => {
   element.forEach((value) => {
     let buttonCell = document.createElement("button");
@@ -53,18 +142,24 @@ arrayOfCell.forEach((element) => {
           buttonCell.innerHTML = " ";
           numberOfRemainBombs += 1;
           countFlag.innerHTML = numberOfRemainBombs;
-          playSoundMove("bomb");
+          if (checkSound) {
+            playSound("bomb");
+          }
         } else if (buttonCell.innerHTML === " ") {
           buttonCell.innerHTML = "🚩";
           numberOfRemainBombs -= 1;
           countFlag.innerHTML = numberOfRemainBombs;
-          playSoundMove("bomb");
+          if (checkSound) {
+            playSound("bomb");
+          }
         } else if (
           buttonCell.innerHTML !== "🚩" &&
           buttonCell.innerHTML !== " "
         ) {
           buttonCell.innerHTML;
-          playSoundMove("bomb");
+          if (checkSound) {
+            playSound("bomb");
+          }
         } else if (numberOfRemainBombs > 0) {
           mesageNoFlag.innerHTML = "You used all the flags!";
         }
@@ -74,7 +169,9 @@ arrayOfCell.forEach((element) => {
           numberOfRemainBombs += 1;
           countFlag.innerHTML = numberOfRemainBombs;
           mesageNoFlag.innerHTML = "";
-          playSoundMove("bomb");
+          if (checkSound) {
+            playSound("bomb");
+          }
         } else {
           mesageNoFlag.innerHTML = "You used all the flags!";
         }
@@ -82,6 +179,8 @@ arrayOfCell.forEach((element) => {
     });
   });
 });
+}
+writtenDom()
 
 let gameArray = [];
 
@@ -97,7 +196,7 @@ function fieldFilling() {
     .concat(arrayBomb)
     .sort(() => Math.random() - 0.5);
 
-  for (let i = 0; i < totalGameArray.length / countBomb; i++) {
+  for (let i = 0; i < totalGameArray.length / width; i++) {
     gameArray[i] = totalGameArray.slice(i * width, i * width + width);
   }
   gameArray = gameArray.map((arrInArr, i, arr) => {
@@ -148,9 +247,9 @@ function fieldFilling() {
 }
 fieldFilling();
 
-let arrayOfDomButton = Array.from(document.querySelectorAll(".cell"));
 
 function openCell() {
+  let arrayOfDomButton = Array.from(document.querySelectorAll(".cell"));
   arrayOfDomButton.forEach((cell, index, array) => {
     cell.addEventListener("click", () => {
       if (arrayOfDomButton.every((cell) => cell.innerHTML === " ")) {
@@ -162,7 +261,9 @@ function openCell() {
       }
       cell.innerHTML = gameArray[index];
       if (cell.innerHTML === "💣") {
-        playSoundMove("game-over");
+        if (checkSound) {
+          playSound("game-over");
+        }
         array.forEach((value, i) => {
           if (gameArray[i] === "💣") {
             value.innerHTML = gameArray[i];
@@ -171,10 +272,11 @@ function openCell() {
         gameOver();
       }
       if (cell.innerHTML === "0") {
-        playSoundMove("one-click");
+        if (checkSound) {
+          playSound("one-click");
+        }
         // cell.innerHTML = "i";
         function openNeighborCells(currentIndex) {
-
           // if (array[currentIndex].classList.contains("checked")) return;
           if (
             array[currentIndex - (width + 1)] &&
@@ -185,21 +287,21 @@ function openCell() {
               gameArray[currentIndex - (width + 1)];
             array[currentIndex - (width + 1)].classList.add("checked");
             if (array[currentIndex - (width + 1)].innerHTML === "0") {
-              console.log("left-top-diagonal", currentIndex);
-               openNeighborCells(currentIndex - (width + 1));
+              // console.log("left-top-diagonal", currentIndex);
+              openNeighborCells(currentIndex - (width + 1));
             }
           }
           if (
             currentIndex > width &&
             !array[currentIndex - width].classList.contains("checked")
           ) {
-            console.log("AAAAAAAAAAA");
+            // console.log("AAAAAAAAAAA");
             array[currentIndex - width].innerHTML =
               gameArray[currentIndex - width];
             array[currentIndex - width].classList.add("checked");
             if (array[currentIndex - width].innerHTML === "0") {
-              console.log("top", currentIndex);
-               openNeighborCells(currentIndex - width);
+              // console.log("top", currentIndex);
+              openNeighborCells(currentIndex - width);
             }
           }
           if (
@@ -210,10 +312,10 @@ function openCell() {
             array[currentIndex - (width - 1)].innerHTML =
               gameArray[currentIndex - (width - 1)];
             array[currentIndex - (width - 1)].classList.add("checked");
-            console.log("right-diagonal", currentIndex);
+            // console.log("right-diagonal", currentIndex);
             if (array[currentIndex - (width - 1)].innerHTML === "0") {
-              console.log("recursive right-top-diagonal");
-               openNeighborCells(currentIndex - (width - 1));
+              // console.log("recursive right-top-diagonal");
+              openNeighborCells(currentIndex - (width - 1));
             }
           }
 
@@ -225,8 +327,8 @@ function openCell() {
             array[currentIndex + 1].innerHTML = gameArray[currentIndex + 1];
             array[currentIndex + 1].classList.add("checked");
             if (array[currentIndex + 1].innerHTML === "0") {
-              console.log("right", currentIndex);
-               openNeighborCells(currentIndex + 1);
+              // console.log("right", currentIndex);
+              openNeighborCells(currentIndex + 1);
             }
           }
 
@@ -239,8 +341,8 @@ function openCell() {
               gameArray[currentIndex + (width + 1)];
             array[currentIndex + (width + 1)].classList.add("checked");
             if (array[currentIndex + (width + 1)].innerHTML === "0") {
-              console.log("bottom-right-diagonal", currentIndex);
-               openNeighborCells(currentIndex + (width + 1));
+              // console.log("bottom-right-diagonal", currentIndex);
+              openNeighborCells(currentIndex + (width + 1));
             }
           }
           if (
@@ -251,8 +353,8 @@ function openCell() {
               gameArray[currentIndex + width];
             array[currentIndex + width].classList.add("checked");
             if (array[currentIndex + width].innerHTML === "0") {
-              console.log("bottom", currentIndex);
-               openNeighborCells(currentIndex + width);
+              // console.log("bottom", currentIndex);
+              openNeighborCells(currentIndex + width);
             }
           }
           if (
@@ -264,8 +366,8 @@ function openCell() {
               gameArray[currentIndex + (width - 1)];
             array[currentIndex + (width - 1)].classList.add("checked");
             if (array[currentIndex + (width - 1)].innerHTML === "0") {
-              console.log("left-bottom-diagonal", currentIndex);
-               openNeighborCells(currentIndex + (width - 1));
+              // console.log("left-bottom-diagonal", currentIndex);
+              openNeighborCells(currentIndex + (width - 1));
             }
           }
           if (
@@ -276,16 +378,16 @@ function openCell() {
             array[currentIndex - 1].innerHTML = gameArray[currentIndex - 1];
             array[currentIndex - 1].classList.add("checked");
             if (array[currentIndex - 1].innerHTML === "0") {
-              console.log("left", currentIndex);
-               openNeighborCells(currentIndex - 1);
+              // console.log("left", currentIndex);
+              openNeighborCells(currentIndex - 1);
             }
           }
           return;
         }
         openNeighborCells(index);
       }
-      if (cell.innerHTML !== "0" && cell.innerHTML !== "💣") {
-        playSoundMove("one-click");
+      if (cell.innerHTML !== "0" && cell.innerHTML !== "💣" && checkSound) {
+        playSound("one-click");
       }
     });
   });
@@ -313,24 +415,12 @@ function gameOver() {
   });
 }
 
-function playSoundMove(typeSound) {
+function playSound(typeSound) {
   let audio = document.createElement("audio");
   audio.src = `./assets/audio/${typeSound}.mp3`;
   audio.autoplay = true;
   return true;
 }
 
-// checkbox
-const wrapperComplexity = document.createElement("div");
-wrapperComplexity.className = "complexity-wrapper";
-body.appendChild(wrapperComplexity);
 
-arrayComplexity.forEach((value) => {
-  const complexity = document.createElement("input");
-  const textComplexity = document.createElement("label");
-  complexity.type = "radio";
-  complexity.className = "buttonComplexity";
-  textComplexity.innerHTML = value;
-  wrapperComplexity.appendChild(complexity);
-  wrapperComplexity.appendChild(textComplexity);
-});
+

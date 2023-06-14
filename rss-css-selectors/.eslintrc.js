1806ce87463d7b9b5c87a600d0e2a46bae20bf99
Anton Viter <antonbntu@gmail.com>
2023-06-14 15:47:26 +0300

    init: start project, add eslint, webpack, html

---
+++ rss-css-selectors/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true ///
+    },
+    "extends": [
+        "eslint:recommended",
+        "airbnb-typescript/base",   ///
+        "plugin:@typescript-eslint/recommended",  ///
+        "plugin:unicorn/recommended",  ///
+        "plugin:prettier/recommended",  ///
+        "prettier",  ///
+        "prettier/@typescript-eslint"  ///
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "prettier",  ///
+        "unicorn" ,  ///
+        "import"     ///
+
+    ],
+    "rules": {
+        "no-debugger": "off", ///
+        "no-console": 0  ///
+    }
+}

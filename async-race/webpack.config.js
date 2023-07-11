22d1176a6391eeafede5c590b788dfe7fd33a7e2
Anton Viter <antonbntu@gmail.com>
2023-07-11 23:39:46 +0300

    init: tuned webpack, installed eslint, started layout

---
+++ async-race/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./index.ts",
+    devtool: "inline-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.svg/,
+          type: "asset/resource",
+        },
+
+        {
+          test: /\.s[ac]ss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "./index.html"),
+      }),
+      new MiniCssExtractPlugin(),
+      new CopyWebpackPlugin({
+        patterns: [{ from: "./assets", to: "assets" }],
+      }),
+    ],
+
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+  };
